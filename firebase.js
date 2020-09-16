1a81d656fb1d2f3525e71a1f1373bca1f5757c61
asghar sheikh <itasghar786@gmail.com>
2020-09-15 21:51:53 -0400

    start of an amazonclone

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBix8ODHyKkQJrTnR4pdsFKx3f09KBIPpY",
+    authDomain: "challange-2debb.firebaseapp.com",
+    databaseURL: "https://challange-2debb.firebaseio.com",
+    projectId: "challange-2debb",
+    storageBucket: "challange-2debb.appspot.com",
+    messagingSenderId: "814365535815",
+    appId: "1:814365535815:web:9142f4cacb2b6e0f3dcb62",
+    measurementId: "G-E4BXDESSDZ"
+};
